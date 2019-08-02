9709ab4aaca6e4f4ba473ed3fbea2f2f896e232b
David Forer <david.j.forer@gmail.com>
2019-08-02 03:47:26 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT LOW(gpa) FROM students;"
 end
 
 def average_student_gpa
+  
 end
 
 def total_tardies_for_all_students
+  
 end
 
 def average_gpa_for_9th_grade
+  
 end
